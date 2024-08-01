886f53f94415a46ad7a7b11edb9a166b4de08a7d
Manas Akarshan <manasakarshan@gmail.com>
2024-08-01 17:56:19 +0530

    Adjusted landing page margin

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <div
-        className=" flex flex-col gap-y-40 min-h-screen"
+        className=" flex flex-col min-h-screen"
         style={{
           background: "#232526",
           background: '-webkit-linear-gradient(to right, #232526, #414345)',
@@ -22,7 +22,7 @@ export default function Home() {
 
         </div>
 
-        <div className="mx-auto max-w-[1280px] flex justify-between gap-20 max-sm:flex-col max-sm:justify-center max-sm:items-center">
+        <div className="mx-auto max-w-[1280px] flex justify-between gap-20 max-sm:flex-col max-sm:justify-center max-sm:items-center lg:mt-16 mt-32">
           {/* Left */}
           <div className="basis-1/2 px-3 flex flex-col justify-center gap-4 max-sm:items-center">
             <p className="text-6xl flex">
